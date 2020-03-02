3f25796b758fd848fdd2adf1637a9af7c301d847
Piotr Kocia <p.kocia@wp.pl>
2020-03-02 20:48:16 +0100

    extension count param in get_vk_extensions

--- public/mimas/mimas_vk.h
+++ public/mimas/mimas_vk.h
@@ -14,7 +14,7 @@ MIMAS_API mimas_bool mimas_init_with_vk();
 /*
  * Returns: A NULL terminated list of vk extension names.
  */
-MIMAS_API char const** mimas_get_vk_extensions();
+MIMAS_API char const** mimas_get_vk_extensions(mimas_i32* extension_count);
 
 /*
  * Returns: VK_SUCCESS on success, VK_ERROR_OUT_OF_HOST_MEMORY or VK_ERROR_OUT_OF_DEVICE_MEMORY on failure.
